5d6673e14262f1aecfe23fc02c44ec0aa4a20259
kurosh <bazrafkan.mb@gmail.com>
2019-11-14 21:42:13 +0100

    fix: change login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,113 +1,77 @@
 import 'package:flutter/material.dart';
-
-const _PATH = "assets/images";
-const _PIC01 = "$_PATH/google_logo.png";
-const _PIC02 = "$_PATH/logoLogin.jpg";
+import 'package:provider/provider.dart';
+import 'package:big_gym_clinte/classes/user.dart';
 
 class LoginPage extends StatelessWidget {
-  final TextEditingController idController = new TextEditingController();
-  final TextEditingController pswController = new TextEditingController();
-
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
     return Container(
-      child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            new Image.asset(_PIC02),
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 11.0, horizontal: 40.0),
-              child: new TextField(
-                controller: idController,
-                decoration: new InputDecoration(
-                  labelText: "Inserisci la tua mail",
-                  fillColor: Colors.white,
-                  prefixIcon: Icon(Icons.mail_outline),
-                  border: new OutlineInputBorder(
-                    borderRadius: new BorderRadius.circular(40.0),
-                    borderSide: new BorderSide(),
-                  ),
-                ),
-                style: new TextStyle(fontSize:18.0,
-                    color: const Color(0xFF000000),
-                    fontWeight: FontWeight.w300,
-                    fontFamily: "Roboto"),
+      color: Colors.blue,
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 8,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Image.asset(
+                'assets/images/logoLogin.jpg',
+                fit: BoxFit.contain,
               ),
             ),
-
-            SizedBox(height: 50,),
-
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 11.0, horizontal: 40.0),
-              child: new TextField(
-                controller: pswController,
-                obscureText: true,
-                decoration: new InputDecoration(
-                    labelText: "Inserisci la Password",
-                    fillColor: Colors.white,
-                    prefixIcon: Icon(Icons.vpn_key),
-                    border: new OutlineInputBorder(
-                      borderRadius: new BorderRadius.circular(40.0),
-                      borderSide: new BorderSide(),
-                    )
-                ),
-                style: new TextStyle(fontSize:18.0,
-                    color: const Color(0xFF000000),
-                    fontWeight: FontWeight.w300,
-                    fontFamily: "Roboto"),
+          ),
+          Expanded(
+            flex: 1,
+            child: RaisedButton(
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
               ),
-            ),
-
-            SizedBox(height: 50,),
-            new Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              onPressed: () async {
+                if (!await user.signIn()) {
+//Show error
+                }
+              },
+              padding:
+                  EdgeInsets.only(top: 5.0, bottom: 5.0, left: 20, right: 20),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
-                  new FlatButton(key:null,
-                      padding: EdgeInsets.symmetric(vertical: 11.0, horizontal: 12.0),
-                      shape: new RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(40.0),
-                          side: BorderSide(color: Colors.blue)),
-                      onPressed: (){
-
-                      },
-                      child:
-                      new Text(
-                        "Accedi",
-                        style: new TextStyle(fontSize:30.0,
-                            color: Color(0xFF2196f3),
-                            fontWeight: FontWeight.w100,
-                            fontFamily: "Roboto"
-                        ),
-                      )
+                  Image.asset('assets/images/google_logo.png'),
+                  SizedBox(
+                    width: 5.0,
                   ),
-                  Padding(padding: EdgeInsets.symmetric(vertical: 11.0, horizontal: 12.0)),
-                  new FlatButton(key:null,
-                      padding: EdgeInsets.symmetric(vertical: 11.0, horizontal: 12.0),
-                      shape: new RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(40.0),
-                          side: BorderSide(color: Colors.blue)),
-                      onPressed: () {
-
-                      },
-                      child: new Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          new Image.asset(
-                            _PIC01,
-                            height: 30.0,
-                          ),
-                          new Container(
-                              padding: EdgeInsets.only(left: 10.0),
-                              child: new Text("Sign in with Google",style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold),)
-                          ),
-                        ],
-                      )
-                  )
-                ]
-            )
-          ]
+                  Container(
+                    width: 1,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Text(
+                    "Sign in with Google",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Container(),
+          )
+        ],
       ),
     );
   }
